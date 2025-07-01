d7f5fec6c38f74fa8ef16ea3961511821dd18956
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2025-07-01 23:24:33 +0300

    add section and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   adaptiveBackground();
   reviewsSliderInit();
 	handlePopup();
-	
+	accordionFunction();
 });
 
 
@@ -154,4 +154,14 @@ const handlePopup = () => {
   openPopup();
   closePopup();
 };
-
+const accordionFunction = () => {
+  const accordionItems = document.querySelectorAll(".accord-item");
+  accordionItems.forEach((item) => {
+		const top = item.querySelector(".accord-item-top");
+		if(top){
+			top.addEventListener("click", function () {
+				item.classList.toggle("active");
+			});
+		}
+  });
+};
