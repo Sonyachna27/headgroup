f0d33e6cead25041523f33af597639045bc06ffa
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2025-07-11 23:24:24 +0300

    add style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   toggleMenu();
-  casesSliderInit();
   adaptiveBackground();
   reviewsSliderInit();
 	handlePopup();
 	accordionFunction();
-	addAnimationInit();
+	addAnimationInit();	
 	triggerScrollInit();
-
+	triggerCasesInit();
 });
+
+
 
 
 const toggleMenu = () => {
@@ -32,33 +33,8 @@ const toggleMenu = () => {
   });
 };
 
-// === Swiper контролери ===
-let casesSwiper = null;
+
 let reviewsSwiper = null;
-
-const casesSliderInit = () => {
-  const casesSliderWrap = document.querySelector(".casesSlider");
-  if (!casesSliderWrap) return;
-
-  const shouldEnable = window.innerWidth >= 1024;
-
-  if (shouldEnable && !casesSwiper) {
-    casesSwiper = new Swiper(casesSliderWrap, {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      pagination: {
-        el: ".cases-pagination",
-        type: "fraction",
-				clickable:true,
-      },
-    });
-  }
-
-  if (!shouldEnable && casesSwiper) {
-    casesSwiper.destroy(true, false);
-    casesSwiper = null;
-  }
-};
 
 const reviewsSliderInit = () => {
   const reviewsSliderWrap = document.querySelector(".reviewsSlider");
@@ -110,18 +86,6 @@ const adaptiveBackground = () => {
   });
 };
 
-
-let resizeTimeout;
-
-function onResize() {
-  clearTimeout(resizeTimeout);
-  resizeTimeout = setTimeout(() => {
-    casesSliderInit();
-   
-  }, 150);
-}
-
-window.addEventListener("resize", onResize);
 
 const handlePopup = () => {
   const openPopup = () => {
@@ -189,73 +153,18 @@ const addAnimationInit = () => {
 	}
 }
 
-// const horizontalScroll = () => {
-//   const process = document.querySelector('.solutions__wrap');
-//   if (process) {
-//     gsap.registerPlugin(ScrollTrigger);
-
-//     let sections = gsap.utils.toArray('.solutions__item');
-// 		const slidesPerRow = 2.5;
-// 		const sectionsCount = sections.length;
-//     if (sections.length > 3) {
-//       ScrollTrigger.matchMedia({
-//         "(min-width: 1024px)": function() {
-         
-// 					process.style.width = `calc(100% * ${sectionsCount})`;
-//           sections.forEach(item => item.style.maxWidth = `500px`);
-          
-//           gsap.to(sections, {
-//             xPercent: -100 * (sectionsCount - slidesPerRow),
-//             ease: "none",
-//             scrollTrigger: {
-//               trigger: process,
-//               scrub: 1,
-//               pin: true,
-//     					markers: true,
-// 							start: "top top",
-// 							 end: () =>
-// 								"+=" +
-// 								(document.querySelector(".solutions__wrap").offsetWidth -
-// 									document.documentElement.clientWidth),
-// 							invalidateOnRefresh: true
-//               // end: () => `+=${(sectionsCount - slidesPerRow) * window.innerWidth / slidesPerRow}`,
-//             }
-//           });
-//         },
-//         "(max-width: 1023.9px)": function() {
-// 					process.style.width = `calc(100% * ${sectionsCount})`;
-//           sections.forEach(item => item.style.width = `100%`);
-//           gsap.to(sections, {
-//             xPercent: -100 * (sections.length - 1),
-//             ease: "none",
-//             scrollTrigger: {
-//               trigger: process,
-//               scrub: 1,
-//               pin: true,
-// 							start: "top top",
-// 							 end: () =>
-// 								"+=" +
-// 								(document.querySelector(".solutions__wrap").offsetWidth -
-// 									document.documentElement.clientWidth),
-// 							invalidateOnRefresh: true
-//               // end: () => "+=" + process.offsetWidth,
-//             }
-//           });
-//         }
-//       });
-//     }
-//   }
-// };
-
 
 const triggerScrollInit = () => {
-  if (window.listGsapTimeInit === true) return;
+	const scrollSolutions = document.querySelectorAll(".solutions");
+	if(!scrollSolutions) return;
+  if (window.solutionsScrollInit === true) return;
+window.solutionsScrollInit = true;
 
   window.listGsapTimeline = [];
   window.listGsapTimeInit = true;
 
-  document.querySelectorAll(".solutions").forEach((block, index) => {
-    const horizontalScrollWrapper = document.querySelector(".solutions__wrap");
+  scrollSolutions.forEach((block, index) => {
+    const horizontalScrollWrapper = block.querySelector(".solutions__wrap");
     const horizontalScrollItems = horizontalScrollWrapper.querySelector(".solutions__items");
     const scrollItems = Array.from(horizontalScrollItems.querySelectorAll(".solutions__item"));
     if (scrollItems.length === 0) return;
@@ -288,44 +197,41 @@ const triggerScrollInit = () => {
   }
 );
 }
+const triggerCasesInit = () => {
+	const casesElements = document.querySelectorAll(".cases.main__cases");
+	if (window.casesScrollInit === true) return;
+	window.casesScrollInit = true;
+    window.listGsapTimeline = [];
+  window.listGsapTimeInit = true;
 
-// const triggerScrollInit = () => {
-// 	gsap.registerPlugin(ScrollTrigger);
+  casesElements.forEach((block, index) => {
+    const horizontalScrollWrapper = block.querySelector(".cases__wrap");
+    const horizontalScrollItems = horizontalScrollWrapper.querySelector(".cases__items");
+    const scrollItems = Array.from(horizontalScrollItems.querySelectorAll(".cases__item"));
+    if (scrollItems.length === 0) return;
 
-// const block = document.querySelector('.solutions');
-// const scrollWrapper = block.querySelector('.solutions__wrap');
-// const scrollItemsContainer = scrollWrapper.querySelector('.solutions__items');
-// const scrollItems = scrollItemsContainer.querySelectorAll('.solutions__item');
+    const x = () => {
+      const scrollItemWidth = scrollItems[0].scrollWidth / 5;			
+      const totalScroll = horizontalScrollItems.scrollWidth - window.innerWidth;
+      const leftOffset = horizontalScrollItems.getBoundingClientRect().left + window.scrollX;
+      return scrollItemWidth + totalScroll + leftOffset;
+    };
 
-// if (block && scrollItems.length > 0) {
-//   // Розрахунок позицій і довжини прокрутки
-//   // let r = -window.innerHeight / 2 + 220;
-//   let r = -window.innerHeight / 2 + 500;
-//   let i = -1 * (scrollWrapper.scrollWidth - window.innerWidth + 20 * scrollItems.length);
-//   let s = scrollWrapper.scrollWidth - window.innerWidth + 160;
-
-//   if (window.innerWidth < 640) {
-//     i = -1 * (scrollWrapper.scrollWidth - window.innerWidth + 10 * scrollItems.length);
-// 		// r = -window.innerHeight / 2 + 240;
-// 		r = -window.innerHeight / 2 + 700;
-// 		s = scrollWrapper.scrollWidth - window.innerWidth + 100;
-//   }
-
-//   // Створення анімації скролу
-//   gsap.to(scrollItems, {
-//     x: () => i,
-//     ease: "none",
-//     scrollTrigger: {
-//       trigger: scrollWrapper,
-//       pin: true,
-//       start: `${r} center`,
-//       scrub: 1,
-//       end: () => `+=${s}`,
-// 			pinSpacing: true,
-// 			// pinSpacing: "margin"
-//       markers: true // можеш увімкнути для дебагу
-//     }
-//   });
-// }
-
-// }
+    gsap.timeline().fromTo(
+      scrollItems,
+      { x: 0 },
+      {
+        x: () => -x(),
+        scrollTrigger: {
+          trigger: horizontalScrollItems,
+          start: "center center",
+          pin: horizontalScrollWrapper,
+          invalidateOnRefresh: true,
+          anticipatePin: 1,
+          scrub: 1,
+          end: () => "+=" + x(),
+        },
+      }
+    );
+  });
+};
