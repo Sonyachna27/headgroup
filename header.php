b82f50bd88d696363433615739bae7d37d9ca500
Misha Kushnirenko <pro100gall@yandex.ru>
2025-07-23 17:33:14 +0300

    fix title

--- header.php
+++ header.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
-    <title><?= YoastSEO()->meta->for_current_page()->context->title ? YoastSEO()->meta->for_current_page()->context->title : the_title() ?></title>
+<title>
+<?php
+if ( function_exists( 'YoastSEO' ) && YoastSEO()->meta->for_current_page()->context->title ) {
+  echo esc_html( YoastSEO()->meta->for_current_page()->context->title );
+} else {
+  echo esc_html( get_the_title() );
+}
+?>
+</title>
     <?php wp_head();?> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
